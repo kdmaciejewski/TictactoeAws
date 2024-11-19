9c19808b295f0a0a8052f3bbf1f7ff2b15cf1373
Krzysztof Maciejewski <94606867+kdmaciejewski@users.noreply.github.com>
2024-11-19 17:34:20 +0100

    generated https certifacte for localhost

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 import basicSsl from '@vitejs/plugin-basic-ssl';
-import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   plugins: [
     react(),
-    mkcert(),
     // basicSsl(), // Enables basic SSL for HTTPS
   ],
   define: {
@@ -19,11 +18,10 @@ export default defineConfig({
     },
   },
   server: {
-    https: true,
-    //     {
-    //   // key: './certs/key.pem', // Path to the SSL private key
-    //   // cert: './certs/cert.crt', // Path to the SSL certificate
-    // },
+    https: {
+      key:fs.readFileSync("./localhost.key"),
+      cert:fs.readFileSync("./localhost.crt")
+    },
     watch: {
       usePolling: true, // Enable polling for changes
     },
@@ -36,3 +34,47 @@ export default defineConfig({
     outDir: 'build',   // Output to 'build' to mimic CRA structure
   },
 });
+
+
+
+
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import basicSsl from '@vitejs/plugin-basic-ssl';
+// import mkcert from 'vite-plugin-mkcert'
+//
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     mkcert(),
+//     // basicSsl(), // Enables basic SSL for HTTPS
+//   ],
+//   define: {
+//     global: 'globalThis',
+//     'process.env': {},
+//   },
+//   resolve: {
+//     alias: {
+//       buffer: 'buffer',
+//     },
+//   },
+//   server: {
+//     https: true,
+//     //     {
+//     //   // key: './certs/key.pem', // Path to the SSL private key
+//     //   // cert: './certs/cert.crt', // Path to the SSL certificate
+//     // },
+//     watch: {
+//       usePolling: true, // Enable polling for changes
+//     },
+//     host: true,        // Listen on all network interfaces
+//     strictPort: true,  // Fail if the port is already in use
+//     port: 3000,        // Set server to use port 3000
+//     open: true,        // Open browser on server start
+//   },
+//   build: {
+//     outDir: 'build',   // Output to 'build' to mimic CRA structure
+//   },
+// });
