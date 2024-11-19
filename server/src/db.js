0f9128e9c5d903f1e64110a09795371df500c171
Krzysztof Maciejewski <94606867+kdmaciejewski@users.noreply.github.com>
2024-11-19 16:26:23 +0100

    react cognito without verification code

--- server/src/db.js
+++ server/src/db.js
@@ -13,6 +13,14 @@ const pool = new Pool({
 export  function createDatabaseSchema(){
 
 }
+
+// CREATE TABLE users (
+//     userid VARCHAR(36) PRIMARY KEY,
+//     username VARCHAR(50) NOT NULL UNIQUE,
+//     email VARCHAR(100) NOT NULL UNIQUE,
+//     password VARCHAR(255) NOT NULL
+// );
+
 // export async function createDatabaseSchema(pool) {
 //     const client = await pool.connect();
 //     try {
--- server/src/index.js
+++ server/src/index.js
@@ -1,32 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import {StreamChat} from "stream-chat";
-import pkg from 'pg'; // Adjusted import
+import pkg from 'pg';
 const {Pool} = pkg;
-import fs from "fs";
-import https from "https";
-import http from "http";
 
 dotenv.config();
-
-const key = fs.readFileSync("./src/certs/key.pem", 'utf8');
-const cert = fs.readFileSync("./src/certs/cert.crt", 'utf8');
-const credentials = {key: key, cert: cert};
 const app = express();
-
-const options = {
-  key: fs.readFileSync("./src/certs/key.pem", 'utf8'),
-  cert: fs.readFileSync("./src/certs/cert.crt", 'utf8'),
-};
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-const api_key = "842k9artxzb2";
-const api_secret = "qmv88zpghbgrty3kj9y86vb9tqeghadagn7mmv2mm5gyd3355j3f2ys6z5u3zyva";
-const serverClient = new StreamChat.getInstance(api_key, api_secret);
 app.use(cors());
 app.use(express.json());
 
@@ -41,11 +24,6 @@ app.use(express.json());
 // });
 app.post("/signup", async (req, res) => {
     const { userId, userUsername, userEmail } = req.body;
-
-    console.log(process.env.DATABASE_URL);
-    pool.connect()
-    .then(() => console.log("Connected to RDS"))
-    .catch(err => console.error("Failed to connect to RDS", err));
 
     if (!userId || !userUsername || !userEmail) {
         return res.status(400).json({ error: "All fields are required." });
@@ -65,8 +43,6 @@ app.post("/signup", async (req, res) => {
             return res.status(200).json({ message: "User already exists." });
         }
 
-        const token = serverClient.createToken(userId);
-        console.log(token)
         res.status(201).json({
             message: "User created successfully",
             user: result.rows[0],
@@ -83,11 +59,7 @@ app.post("/signup", async (req, res) => {
 app.post("/login", async (req, res) => {
     const {username} = req.body;
     try {
-        const {users} = await serverClient.queryUsers({name: username});
         if (users.length === 0) return res.status(404).json({message: "User not found"});
-
-        const token = serverClient.createToken(users[0].id);
-        res.json({token, userId: users[0].id}); // Send userId along with the token
     } catch (error) {
         res.status(500).json(error);
     }
